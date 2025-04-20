4e92ce74157a4c031cd18bafeb37be84e7c581f6
SushantBajaj <sushant1108bajaj@gmail.com>
2025-04-20 15:30:00 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header-logo">Sorting Visualizer</div>
+    </div>
+  );
+}
+
+export default Header;
